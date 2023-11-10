4a7ede02ac069374b683ff0a7c84a2f42d8e19b1
ranwar26 <ranwar26@students.holmen.k12.wi.us>
2023-11-09 19:44:31 -0600

    Added drive method and motor setup

--- src/main/java/frc/robot/subsystems/drive/DriveIOReal.java
+++ src/main/java/frc/robot/subsystems/drive/DriveIOReal.java
@@ -11,20 +11,42 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 /** Add your docs here. */
 public class DriveIOReal {
 
-    private WPI_TalonSRX m_talonL;
-    private WPI_TalonSRX m_talonR;
+    private WPI_TalonSRX m_talonFrontLeft = new WPI_TalonSRX(0);
+    private WPI_TalonSRX m_talonBackLeft = new WPI_TalonSRX(1);
+
+    private WPI_TalonSRX m_talonFrontRight = new WPI_TalonSRX(2);
+    private WPI_TalonSRX m_talonBackRight = new WPI_TalonSRX(3);
+
+    private DifferentialDrive m_DifferentialDrive = new DifferentialDrive(m_talonFrontLeft, m_talonFrontRight);
 
     public DriveIOReal() {
 
-        this.m_talonL = new WPI_TalonSRX(1);
-        this.m_talonR = new WPI_TalonSRX(0);
+        this.m_talonFrontLeft.configFactoryDefault();
+        this.m_talonBackLeft.configFactoryDefault();
+        this.m_talonFrontRight.configFactoryDefault();
+        this.m_talonBackRight.configFactoryDefault();
 
-        this.m_talonL.configFactoryDefault();
-        this.m_talonR.configFactoryDefault();
+        this.m_talonFrontRight.setInverted(true);
+        this.m_talonFrontLeft.setInverted(false);
 
-        this.m_talonL.setInverted(false);
-        this.m_talonR.setInverted(true);
+        this.m_talonBackRight.follow(m_talonFrontRight);
+        this.m_talonBackLeft.follow(m_talonFrontLeft);
 
+        this.m_talonFrontRight.configOpenloopRamp(0.5);
+        this.m_talonFrontLeft.configOpenloopRamp(0.5);
+        this.m_talonFrontRight.configClosedloopRamp(0.5);
+        this.m_talonFrontLeft.configClosedloopRamp(0.5);
+    }
+
+    /*
+     * Drive method for drivetrain
+     *
+     * @param xSpeed       The robot's speed along the X axis
+     * @param zRotation    The robot's rotation rate around the Z axis
+     * @param squareInputs Decreases the input sensitivity at low speeds.
+     */
+    public void drive(double xSpeed, double zRotation, boolean isSquare) {
+        m_DifferentialDrive.arcadeDrive(xSpeed, zRotation, isSquare);
     }
 
 }
