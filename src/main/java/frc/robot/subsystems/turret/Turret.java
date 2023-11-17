6323bc309eb64bfb6a97b25a106cf12343e9faa4
OwenMShaw <shaowe26@students.holmen.k12.wi.us>
2023-11-16 19:03:42 -0600

    Started work on turret functions.

--- src/main/java/frc/robot/subsystems/turret/Turret.java
+++ src/main/java/frc/robot/subsystems/turret/Turret.java
@@ -4,9 +4,16 @@
 
 package frc.robot.subsystems.turret;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Turret extends SubsystemBase {
+
+private WPI_TalonSRX m_RotatationMotor = new WPI_TalonSRX(0);
+private WPI_TalonSRX m_AngleMotor = new WPI_TalonSRX(1);
+
   /** Creates a new Turret. */
   public Turret() {}
 
